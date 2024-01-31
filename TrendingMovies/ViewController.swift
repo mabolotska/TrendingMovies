89f8b377cfa125302114dbba47116631ea44c373
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2024-01-31 11:55:05 +0100

    Initial Commit

---
+++ TrendingMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrendingMovies
+//
+//  Created by Maryna Bolotska on 31/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
